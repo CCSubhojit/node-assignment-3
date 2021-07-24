872a6499199737ea5e2c39db91a26938524c3866
Subhojit Sinha <subhojit.sinha@codeclouds.in>
2021-07-24 18:22:30 +0530

    Making port dynamic for heroku

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ mongoose
 
 // Starting the node server on port 3000
 const port = 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App running on port ${port}...`);
 });
